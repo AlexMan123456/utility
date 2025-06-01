1234f390d7a882072b5b647479e25e4c9fa6678d
AlexMan123456 <alex.man03888@gmail.com>
2025-06-01 16:31:52 +0100

    Add getRandomNumber function

---
+++ __tests__/get-random-number.test.ts
@@ -0,0 +1,20 @@
+import getRandomNumber from "../src/get-random-number"
+
+describe("getRandomNumber", () => {
+    test("Returns an integer", () => {
+        const randomNumber = getRandomNumber(0,10);
+        expect(randomNumber%1).toBe(0);
+    })
+    test("Generates a random number within the specified range", () => {
+        const randomNumber = getRandomNumber(0,10);
+        expect(randomNumber >= 0 && randomNumber <= 10).toBe(true);
+    })
+    test("Throw an error if inputs are not integers", () => {
+        try {
+            getRandomNumber(0.5,10.5);
+            throw new Error("Test failed");
+        } catch(error: any) {
+            expect(error?.message).toBe("NON_INTEGER_INPUTS");
+        }
+    })
+})
