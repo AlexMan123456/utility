b5ec54997f0a7e571393c33fb8ea9690764b9db2
AlexMan123456 <alex.man03888@gmail.com>
2025-06-07 01:54:19 +0100

    Add addDaysToDate to index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as getRandomNumber } from "./get-random-number";
 export { default as randomiseArray } from "./randomise-array";
 export { default as wait } from "./wait";
 export { default as formatDateAndTime } from "./format-date-and-time";
+export { default as addDaysToDate } from "./add-days-to-date";
