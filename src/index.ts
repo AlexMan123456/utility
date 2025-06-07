29bc881370ac8261352f329584fa4ba701022b71
AlexMan123456 <alex.man03888@gmail.com>
2025-06-07 01:49:39 +0100

    Export formatDateAndTime from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as getRandomNumber } from "./get-random-number";
 export { default as randomiseArray } from "./randomise-array";
 export { default as wait } from "./wait";
+export { default as formatDateAndTime } from "./format-date-and-time";
