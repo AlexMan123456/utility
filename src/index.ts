50b002ee0a6ed6c07d43b2b9fa1c1ab93f45c8c9
AlexMan123456 <alex.man03888@gmail.com>
2025-06-14 17:15:02 +0100

    Export truncate function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as randomiseArray } from "./randomise-array";
 export { default as wait } from "./wait";
 export { default as formatDateAndTime } from "./format-date-and-time";
 export { default as addDaysToDate } from "./add-days-to-date";
+export { default as truncate } from "./truncate";
