afede6d45b9f0eff604d0d8fcf35011e07b8dbeb
AlexMan123456 <alex.man03888@gmail.com>
2025-07-13 12:41:02 +0100

    Get rid of globals since it's used in the base config now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,3 @@
 import alexBaseConfig from "@alextheman/eslint-config-typescript-base";
-import globals from "globals";
 
-export default [
-  ...alexBaseConfig,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-        ...globals.node
-      },
-    },
-  },
-];
+export default alexBaseConfig;
