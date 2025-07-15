33a7176a65df2859fc18d2dd44693705e9105b02
AlexMan123456 <alex.man03888@gmail.com>
2025-07-16 00:31:45 +0100

    Export appendSemicolon

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as wait } from "src/wait";
 export { default as formatDateAndTime } from "src/format-date-and-time";
 export { default as addDaysToDate } from "src/add-days-to-date";
 export { default as truncate } from "src/truncate";
+export { default as appendSemicolon } from "src/append-semicolon";
