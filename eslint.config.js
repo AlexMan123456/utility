cd2cc31fc347ca494fb411ef981a3bcc3d81f4b2
AlexMan123456 <alex.man03888@gmail.com>
2025-08-17 01:23:34 +0100

    Add ESLint rule to require function return types to be specified if exported
    I only want to enforce this in utility as it feels unnecessary in packages such as components or AlexCLine where functions aren't necessarily the main purpose (components almost always returns a ReactNode and AlexCLine isn't even a tool to be used in TypeScript/JavaScript code). But with utility, the aim is to provide functions to be used, and as such, we want to be as explicit as possible about how to use them. Explicitly specifying the return type helps with this.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,13 @@
 import plugin from "@alextheman/eslint-plugin";
 
-export default plugin.configs.alexTypeScriptBase;
+export default [
+    ...plugin.configs.alexTypeScriptBase,
+    {
+        rules: {
+            /* Since the entire purpose of this package is to provide functions to be used by others, it's best to be explicit about the exact return type
+            we expect from them. It's not as important in most other packages since they tend to be more predictable, but for utility, where the entire
+            purpose of the package is functions, it's best to have the return type specified. */
+            "@typescript-eslint/explicit-module-boundary-types": "error"
+        }
+    }
+];
