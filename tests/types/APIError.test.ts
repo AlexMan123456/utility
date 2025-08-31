ef025c96a3d6df31fbae29cb3b40abcfa2eaec66
AlexMan123456 <alex.man03888@gmail.com>
2025-08-31 01:50:48 +0100

    Use test.each in APIError default messages tests

--- tests/types/APIError.test.ts
+++ tests/types/APIError.test.ts
@@ -19,25 +19,16 @@ describe("APIError", () => {
   test("Throws an error with the given status and message", () => {
     testAPIError(new APIError(400, "TEST_PASSED"), 400, "TEST_PASSED");
   });
-  describe("Give sensible default messages to common error status codes", () => {
-    test("400: BAD_REQUEST", () => {
-      testAPIError(new APIError(400), 400, "BAD_REQUEST");
-    });
-    test("401: UNAUTHORISED", () => {
-      testAPIError(new APIError(401), 401, "UNAUTHORISED");
-    });
-    test("403: FORBIDDEN", () => {
-      testAPIError(new APIError(403), 403, "FORBIDDEN");
-    });
-    test("404: NOT_FOUND", () => {
-      testAPIError(new APIError(404), 404, "NOT_FOUND");
-    });
-    test("418: I_AM_A_TEAPOT", () => {
-      testAPIError(new APIError(418), 418, "I_AM_A_TEAPOT");
-    });
-    test("500: INTERNAL_SERVER_ERROR", () => {
-      testAPIError(new APIError(500), 500, "INTERNAL_SERVER_ERROR");
-    });
+  const withTestCases = test.each([
+    [400, "BAD_REQUEST"],
+    [401, "UNAUTHORISED"],
+    [403, "FORBIDDEN"],
+    [404, "NOT_FOUND"],
+    [418, "I_AM_A_TEAPOT"],
+    [500, "INTERNAL_SERVER_ERROR"],
+  ]);
+  withTestCases("For status code %s, give default message %s", (status, message) => {
+    testAPIError(new APIError(status), status, message);
   });
   test("Default to 500: INTERNAL_SERVER_ERROR if no status or message provided", () => {
     testAPIError(new APIError(), 500, "INTERNAL_SERVER_ERROR");
