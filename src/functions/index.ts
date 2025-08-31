f42f2f850d34f60a13132f1670d5cb81769d3427
AlexMan123456 <alex.man03888@gmail.com>
2025-08-31 15:30:51 +0100

    Add fillArray function
    This gets rid of the need to constantly declare new Array().fill(null).map() every time. Now we can just call fillArray with a function to evaluate to fill an array.

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,7 @@
 export { default as addDaysToDate } from "src/functions/addDaysToDate";
 export { default as appendSemicolon } from "src/functions/appendSemicolon";
 export { default as convertFileToBase64 } from "src/functions/convertFileToBase64";
+export { default as fillArray } from "src/functions/fillArray";
 export { default as formatDateAndTime } from "src/functions/formatDateAndTime";
 export { default as getRandomNumber } from "src/functions/getRandomNumber";
 export { default as isLeapYear } from "src/functions/isLeapYear";
