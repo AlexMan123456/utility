1b61e4c8ff58f5fe770e283fd314e2899d536794
AlexMan123456 <alex.man03888@gmail.com>
2025-10-29 23:45:22 +0000

    Improve mutation checks by freezing inputs in tests
    Instead of passing in the array directly, we freeze it first, then pass it in. This allows us to check for any attempts at all to mutate the input array, even if it does ultimately return the same contents after testing.

--- src/functions/omitProperties.ts
+++ src/functions/omitProperties.ts
@@ -1,13 +1,13 @@
-function omitProperties<T extends Record<string, unknown>, K extends keyof T>(
-  object: T,
-  keysToOmit: K | readonly K[],
-): Omit<T, K> {
-  const outputObject = { ...object };
+function omitProperties<
+  T extends Record<string, unknown> | Readonly<Record<string, unknown>>,
+  K extends keyof T,
+>(object: T, keysToOmit: K | readonly K[]): Omit<T, K> {
+  const outputObject: Record<string, unknown> = { ...object };
   const keysArray = Array.isArray(keysToOmit) ? keysToOmit : [keysToOmit];
   keysArray.forEach((key) => {
     delete outputObject[key];
   });
-  return outputObject;
+  return outputObject as Omit<T, K>;
 }
 
 export default omitProperties;
--- tests/functions/omitProperties.test.ts
+++ tests/functions/omitProperties.test.ts
@@ -23,44 +23,48 @@ describe("omitProperties", () => {
       fourthKey: "Fourth property",
     });
   });
-  test("Does not mutate the input object", () => {
-    const inputObject = {
-      firstKey: "First property",
-      secondKey: "Second property",
-      thirdKey: "Third property",
-      fourthKey: "Fourth property",
-    };
-    omitProperties(inputObject, "firstKey");
-    expect(inputObject).toEqual({
-      firstKey: "First property",
-      secondKey: "Second property",
-      thirdKey: "Third property",
-      fourthKey: "Fourth property",
-    });
-  });
-  test("Does not mutate the array of keys", () => {
-    const keys = ["firstKey", "thirdKey"] as const;
-    omitProperties(
-      {
+  describe("Mutation checks", () => {
+    test("Does not mutate the input object", () => {
+      const inputObject = Object.freeze({
         firstKey: "First property",
         secondKey: "Second property",
         thirdKey: "Third property",
         fourthKey: "Fourth property",
-      },
-      keys,
-    );
-    expect(keys).toEqual(["firstKey", "thirdKey"]);
-  });
-  test("Returns an object with a new reference in memory", () => {
-    const keys = ["firstKey", "thirdKey"] as const;
-    const inputObject = {
-      firstKey: "First property",
-      secondKey: "Second property",
-      thirdKey: "Third property",
-      fourthKey: "Fourth property",
-    };
-    const outputObject = omitProperties(inputObject, keys);
-    expect(outputObject).not.toBe(keys);
-    expect(outputObject).not.toBe(inputObject);
+      });
+      // Will error on mutation attempt due to freeze.
+      omitProperties(inputObject, "firstKey");
+      expect(inputObject).toEqual({
+        firstKey: "First property",
+        secondKey: "Second property",
+        thirdKey: "Third property",
+        fourthKey: "Fourth property",
+      });
+    });
+    test("Does not mutate the array of keys", () => {
+      const keys = Object.freeze(["firstKey", "thirdKey"] as const);
+      // Will error on mutation attempt due to freeze.
+      omitProperties(
+        {
+          firstKey: "First property",
+          secondKey: "Second property",
+          thirdKey: "Third property",
+          fourthKey: "Fourth property",
+        },
+        keys,
+      );
+      expect(keys).toEqual(["firstKey", "thirdKey"]);
+    });
+    test("Returns an object with a new reference in memory", () => {
+      const keys = ["firstKey", "thirdKey"] as const;
+      const inputObject = {
+        firstKey: "First property",
+        secondKey: "Second property",
+        thirdKey: "Third property",
+        fourthKey: "Fourth property",
+      };
+      const outputObject = omitProperties(inputObject, keys);
+      expect(outputObject).not.toBe(keys);
+      expect(outputObject).not.toBe(inputObject);
+    });
   });
 });
