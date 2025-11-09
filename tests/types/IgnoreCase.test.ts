df2640de2ab46869798504b2bdee28ced4665b96
AlexMan123456 <alex.man03888@gmail.com>
2025-11-09 01:58:04 +0000

    Add IgnoreCase type
    This is useful if we ever need to accept a literal string of some sort but we don't care about the case.

---
+++ tests/types/IgnoreCase.test.ts
@@ -0,0 +1,16 @@
+import type { IgnoreCase } from "src/types";
+
+import { describe, expectTypeOf, test } from "vitest";
+
+describe("IgnoreCase", () => {
+  test("Matches the input type exactly when lowercase", () => {
+    expectTypeOf<"hello">().toExtend<IgnoreCase<"hello">>();
+  });
+  test("Matches the input type exactly when uppercasecase", () => {
+    expectTypeOf<"hello">().toExtend<IgnoreCase<"HELLO">>();
+  });
+  test("Ignores the case of the input type", () => {
+    expectTypeOf<"hello">().toExtend<IgnoreCase<"HELlo">>();
+    expectTypeOf<"HELLO">().toExtend<IgnoreCase<"HELlo">>();
+  });
+});
