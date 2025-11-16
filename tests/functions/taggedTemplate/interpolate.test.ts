b79bb223b55f6c75330defb4e337817221925b89
AlexMan123456 <alex.man03888@gmail.com>
2025-11-16 20:24:30 +0000

    Add interpolate function
    This takes in a template strings array along with the interpolations, and returns the resulting string. If the input itself is a template string, that template string itself is therefore returned.

---
+++ tests/functions/taggedTemplate/interpolate.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+
+import { createTemplateStringsArray, interpolate } from "src/index";
+
+describe("interpolate", () => {
+  test("Returns the same template string when given a template string", () => {
+    expect(interpolate`Hello ${"world"} test ${1}.`).toBe("Hello world test 1.");
+  });
+  test("Returns the resulting template string when given the strings and interpolations separately", () => {
+    expect(interpolate(createTemplateStringsArray(["Hello ", "test ", "."]), "world ", 1)).toBe(
+      "Hello world test 1.",
+    );
+  });
+});
