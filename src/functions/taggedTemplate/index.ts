ecda185ccb1f8e5174548d7341ca674b3de6e22b
AlexMan123456 <alex.man03888@gmail.com>
2025-11-25 21:40:47 +0900

    Remove deprecations
    This feels like a good time to change the major version, so let's also get rid of deprecated features while we're at it.

--- src/functions/taggedTemplate/index.ts
+++ src/functions/taggedTemplate/index.ts
@@ -1,13 +1,8 @@
 export { default as interpolate } from "src/functions/taggedTemplate/interpolate";
 export { default as interpolateObjects } from "src/functions/taggedTemplate/interpolateObjects";
 export { default as removeIndents } from "src/functions/taggedTemplate/removeIndents";
-export { default as stripIndents } from "src/functions/taggedTemplate/stripIndents";
 
 export type {
   RemoveIndentsOptions,
   RemoveIndentsFunction,
 } from "src/functions/taggedTemplate/removeIndents";
-export type {
-  StripIndentsOptions,
-  StripIndentsFunction,
-} from "src/functions/taggedTemplate/stripIndents";
