1da7a356771635b04ef9df06b71aa3967d7a91ee
AlexMan123456 <alex.man03888@gmail.com>
2025-12-09 19:27:50 +0000

    Refactor to use deepCopy function

--- src/functions/omitProperties.ts
+++ src/functions/omitProperties.ts
@@ -1,8 +1,10 @@
+import deepCopy from "src/functions/deepCopy";
+
 function omitProperties<
   T extends Record<string, unknown> | Readonly<Record<string, unknown>>,
   K extends keyof T,
 >(object: T, keysToOmit: K | readonly K[]): Omit<T, K> {
-  const outputObject: Record<string, unknown> = { ...object };
+  const outputObject: Record<string, unknown> = deepCopy(object);
   const keysArray = Array.isArray(keysToOmit) ? keysToOmit : [keysToOmit];
   keysArray.forEach((key) => {
     delete outputObject[key];
