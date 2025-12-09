8c08db022b8b5598a08c7e27a9e8f360a5c83030
AlexMan123456 <alex.man03888@gmail.com>
2025-12-09 20:13:39 +0000

    Revert "Refactor to use deepCopy function"
    This reverts commit 1da7a356771635b04ef9df06b71aa3967d7a91ee.
    
    This was breaking my ESLint plugin, so I am reverting it.

--- src/functions/omitProperties.ts
+++ src/functions/omitProperties.ts
@@ -1,10 +1,8 @@
-import deepCopy from "src/functions/deepCopy";
-
 function omitProperties<
   T extends Record<string, unknown> | Readonly<Record<string, unknown>>,
   K extends keyof T,
 >(object: T, keysToOmit: K | readonly K[]): Omit<T, K> {
-  const outputObject: Record<string, unknown> = deepCopy(object);
+  const outputObject: Record<string, unknown> = { ...object };
   const keysArray = Array.isArray(keysToOmit) ? keysToOmit : [keysToOmit];
   keysArray.forEach((key) => {
     delete outputObject[key];
