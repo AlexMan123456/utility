270f6ff7fd537e71c377b38fa56afa4fcf03e5e3
AlexMan123456 <alex.man03888@gmail.com>
2025-12-11 23:52:14 +0000

    Add JSDoc comments to a few functions
    I think we should start making it a requirement in all my packages that any exported functions must have JSDoc comments, as that will help improve the developer experience by giving developers immediate guidance on how to use these utilities. That said, I have admittedly been putting this off a bit, so I am currently going through them all to add these in.
    
    Note that for overloaded functions, this is a bit suboptimal as I have to provide JSDoc comments on each individual overload, which is a bit annoying. Not sure if there's a better way, though.

--- src/functions/arrayHelpers/paralleliseArrays.ts
+++ src/functions/arrayHelpers/paralleliseArrays.ts
@@ -1,8 +1,22 @@
 export type ParallelTuple<A, B> = [A, B | undefined];
 
+/**
+ * Creates a new array of tuples, each containing the item at the given index from both arrays.
+ *
+ * If `secondArray` is shorter than `firstArray`, the second position in the tuple
+ * will be `undefined`. Iteration always uses the length of the first array.
+ *
+ * @template FirstArrayItem
+ * @template SecondArrayItem
+ *
+ * @param firstArray - The first array. Each item in this will take up the first tuple spot.
+ * @param secondArray - The second array. Each item in this will take up the second tuple spot.
+ *
+ * @returns An array of `[firstItem, secondItem]` tuples for each index in `firstArray`.
+ */
 function paralleliseArrays<FirstArrayItem, SecondArrayItem>(
-  firstArray: FirstArrayItem[] | readonly FirstArrayItem[],
-  secondArray: SecondArrayItem[] | readonly SecondArrayItem[],
+  firstArray: readonly FirstArrayItem[],
+  secondArray: readonly SecondArrayItem[],
 ): ParallelTuple<FirstArrayItem, SecondArrayItem>[] {
   const outputArray: ParallelTuple<FirstArrayItem, SecondArrayItem>[] = [];
 
