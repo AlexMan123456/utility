9fe13bbde6c5b4e7bc981d0edf2a50b363ea89ce
AlexMan123456 <alex.man03888@gmail.com>
2025-12-14 15:43:01 +0000

    Add JSDoc comments to the parser utilities
    This should provide a better developer experience for people using these shared components, as it gives usage information right there in the editor.

--- src/functions/parsers/parseBoolean.ts
+++ src/functions/parsers/parseBoolean.ts
@@ -1,3 +1,12 @@
+/**
+ * Takes a stringly-typed boolean and converts it to an actual boolean type.
+ *
+ * @param inputString - The string to parse.
+ *
+ * @throws {TypeError} If the string is not either `true` or `false` (case insensitive).
+ *
+ * @returns The string parsed as an actual boolean.
+ */
 function parseBoolean(inputString: string): boolean {
   const normalisedString = inputString.toLowerCase();
   if (!["true", "false"].includes(normalisedString)) {
