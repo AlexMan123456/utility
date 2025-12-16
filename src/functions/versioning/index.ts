3ca41704145471a46c5ed679f75d688b5051697b
AlexMan123456 <alex.man03888@gmail.com>
2025-12-16 13:45:25 +0000

    Move the VersionType to src/types
    It is probably going to be used in other functions beyond just determineVersionType and should therefore not be too tightly coupled with it.

--- src/functions/versioning/index.ts
+++ src/functions/versioning/index.ts
@@ -1,5 +1,3 @@
 export { default as determineVersionType } from "src/functions/versioning/determineVersionType";
 export { default as getIndividualVersionNumbers } from "src/functions/versioning/getIndividualVersionNumbers";
 export { default as parseVersion } from "src/functions/versioning/parseVersion";
-
-export type { VersionType } from "src/functions/versioning/determineVersionType";
