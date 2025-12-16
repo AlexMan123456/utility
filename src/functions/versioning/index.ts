7363cb7386cdebb072e0abe3601a56a710c5596b
AlexMan123456 <alex.man03888@gmail.com>
2025-12-16 13:47:10 +0000

    Export the ParseVersionOptions type

--- src/functions/versioning/index.ts
+++ src/functions/versioning/index.ts
@@ -1,3 +1,5 @@
 export { default as determineVersionType } from "src/functions/versioning/determineVersionType";
 export { default as getIndividualVersionNumbers } from "src/functions/versioning/getIndividualVersionNumbers";
 export { default as parseVersion } from "src/functions/versioning/parseVersion";
+
+export type { ParseVersionOptions } from "src/functions/versioning/parseVersion";
