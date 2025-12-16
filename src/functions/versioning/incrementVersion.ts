2bfa7a21fbb6b73859c7b9067153677fb17d99da
AlexMan123456 <alex.man03888@gmail.com>
2025-12-16 14:06:36 +0000

    Add an incrementVersion function
    This one may end up being used in AlexCLine to determine what the next version should be without changing the version number directly in package.json. In my workflow, the package.json change should be triggered by the GitHub Action, and not the user.

---
+++ src/functions/versioning/incrementVersion.ts
@@ -0,0 +1,39 @@
+import type { VersionType } from "src/types";
+
+import getIndividualVersionNumbers from "src/functions/versioning/getIndividualVersionNumbers";
+import parseVersion from "src/functions/versioning/parseVersion";
+
+export interface IncrementVersionOptions {
+  /** Whether to omit the `v` prefix from the output version or not. */
+  omitPrefix?: boolean;
+}
+
+/**
+ * Increments the given input version depending on the given increment type.
+ *
+ * @param version - The version to increment
+ * @param incrementType - The type of increment. Can be one of the following:
+ * - `"major"`: Change the major version `v1.2.3` → `v2.0.0`
+ * - `"minor"`: Change the minor version `v1.2.3` → `v1.3.0`
+ * - `"patch"`: Change the patch version `v1.2.3` → `v1.2.4`
+ * @param options - Extra options to apply.
+ *
+ * @returns A new string representing the version with the increment applied.
+ */
+function incrementVersion(
+  version: string,
+  incrementType: VersionType,
+  options?: IncrementVersionOptions,
+): string {
+  const [major, minor, patch] = getIndividualVersionNumbers(version);
+
+  const newVersion = {
+    major: `${major + 1}.0.0`,
+    minor: `${major}.${minor + 1}.0`,
+    patch: `${major}.${minor}.${patch + 1}`,
+  }[incrementType];
+
+  return parseVersion(newVersion, { omitPrefix: options?.omitPrefix });
+}
+
+export default incrementVersion;
