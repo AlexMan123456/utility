eff6b22abc2c41b3c13907bb6760f52853383339
AlexMan123456 <alex.man03888@gmail.com>
2025-12-23 12:30:48 +0000

    Make the Env type a const object
    This allows it to also be used as if it were an enum, so pure JavaScript users can also do Env.TEST etc.

--- src/functions/parsers/index.ts
+++ src/functions/parsers/index.ts
@@ -1,8 +1,6 @@
 export { default as parseBoolean } from "src/functions/parsers/parseBoolean";
-export { default as parseEnv } from "src/functions/parsers/parseEnv";
+export { default as parseEnv, Env } from "src/functions/parsers/parseEnv";
 export { default as parseFormData } from "src/functions/parsers/parseFormData";
 export { default as parseIntStrict } from "src/functions/parsers/parseIntStrict";
 export { default as parseVersionType } from "src/functions/parsers/parseVersionType";
 export { default as parseZodSchema } from "src/functions/parsers/parseZodSchema";
-
-export type { Env } from "src/functions/parsers/parseEnv";
