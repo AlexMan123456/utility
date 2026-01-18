36152d459ca80eb33a1bdca1211a881d8a6264f0
AlexMan123456 <alex.man03888@gmail.com>
2026-01-18 20:19:31 +0000

    Add removeUndefinedFromObject helper
    This will be needed in alex-c-line to fix a parsing error.

--- src/functions/objectHelpers/index.ts
+++ src/functions/objectHelpers/index.ts
@@ -1,2 +1,5 @@
 export { default as getRecordKeys } from "src/functions/objectHelpers/getRecordKeys";
 export { default as omitProperties } from "src/functions/objectHelpers/omitProperties";
+export { default as removeUndefinedFromObject } from "src/functions/objectHelpers/removeUndefinedFromObject";
+
+export type { RemoveUndefined } from "src/functions/objectHelpers/removeUndefinedFromObject";
---
+++ tests/functions/objectHelpers/removeUndefinedFromObject.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect, test } from "vitest";
+
+import removeUndefinedFromObject from "src/functions/objectHelpers/removeUndefinedFromObject";
+
+describe("removeUndefinedFromObject", () => {
+  test("Leaves an object as is if there are no undefined entries", () => {
+    const input = {
+      first: "First",
+      second: 2,
+    };
+
+    expect(removeUndefinedFromObject(input)).toEqual(input);
+  });
+
+  test("Removes undefined entries from a given object", () => {
+    const input = {
+      first: "First",
+      second: 2,
+      isUndefined: undefined,
+    };
+
+    expect(removeUndefinedFromObject(input)).toEqual({
+      first: "First",
+      second: 2,
+    });
+  });
+
+  test("Does not mutate the input", () => {
+    const input = Object.freeze({
+      first: "First",
+      second: 2,
+      isUndefined: undefined,
+    });
+
+    removeUndefinedFromObject(input);
+
+    expect(input).toEqual({
+      first: "First",
+      second: 2,
+      isUndefined: undefined,
+    });
+  });
+
+  test("Returns an object with a different reference in memory", () => {
+    const input = {
+      first: "First",
+      second: 2,
+    };
+
+    const output = removeUndefinedFromObject(input);
+
+    expect(input).not.toBe(output);
+  });
+});
